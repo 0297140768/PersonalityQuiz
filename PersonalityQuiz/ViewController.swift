61b6d4619a3a9cb7f60ddf0636373284e62f6b90
Татьяна Гончарова <tanja211030@yandex.ru>
2018-09-08 10:38:41 +0100

    Initial Commit

---
+++ PersonalityQuiz/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PersonalityQuiz
+//
+//  Created by Татьяна on 08.09.2018.
+//  Copyright © 2018 Татьяна. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
